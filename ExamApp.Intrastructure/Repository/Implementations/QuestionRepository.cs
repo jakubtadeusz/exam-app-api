07f78c292b0659ac0e88731c6effe3f97f425755
taydexx <jakub.tadeusz@interia.pl>
2022-11-03 21:17:59 +0100

    Implement repositories

---
+++ ExamApp.Intrastructure/Repository/Implementations/QuestionRepository.cs
@@ -0,0 +1,65 @@
+﻿using ExamApp.Domain.Models;
+using ExamApp.Intrastructure.Context;
+using ExamApp.Intrastructure.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamApp.Intrastructure.Repository.Implementations
+{
+    public class QuestionRepository : IQuestionRepository
+    {
+        private readonly ExamDbContext _context;
+
+        public QuestionRepository(ExamDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Question> AddQuestionAsync(Question question)
+        {
+            await _context.Questions.AddAsync(question);
+            await _context.SaveChangesAsync();
+
+            return question;
+        }
+
+        public async Task DeleteQuestionAsync(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+
+            if (question == null)
+            {
+                return;
+            }
+            _context.Questions.Remove(question);
+            await _context.SaveChangesAsync();
+
+            return;
+        }
+
+        public async Task<Question> GetQuestionByIdAsync(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+
+            if (question == null)
+            {
+                throw new KeyNotFoundException($"Question id {id} not found");
+            }
+            return question;
+        }
+
+        public async Task<IEnumerable<Question>> GetQuestionsAsync(int examId)
+        {
+            var questions = await _context.Questions.Where(q => q.ExamId == examId).ToListAsync();
+
+            return questions;
+        }
+
+        public async Task<Question> UpdateQuestionAsync(Question question)
+        {
+            _context.Entry(question).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return question;
+        }
+    }
+}
---
+++ ExamApp.Intrastructure/Repository/Implementations/ResultRepository.cs
@@ -0,0 +1,70 @@
+﻿using ExamApp.Domain.Models;
+using ExamApp.Intrastructure.Context;
+using ExamApp.Intrastructure.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp.Intrastructure.Repository.Implementations
+{
+    public class ResultRepository : IResultRepository
+    {
+        private readonly ExamDbContext _context;
+
+        public ResultRepository(ExamDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result> CreateResultAsync(Result result)
+        {
+            await _context.Results.AddAsync(result);
+            await _context.SaveChangesAsync();
+
+            return result;
+        }
+
+        public async Task DeleteResultAsync(int id)
+        {
+            var result = await _context.Results.FindAsync(id);
+
+            if (result == null)
+            {
+                return;
+            }
+            _context.Results.Remove(result);
+            await _context.SaveChangesAsync();
+
+            return;
+        }
+
+        public async Task<Result> GetResultByIdAsync(int id)
+        {
+            var result = await _context.Results.FindAsync(id);
+
+            if (result == null)
+            {
+                throw new KeyNotFoundException($"Result id {id} not found");
+            }
+            return result;
+        }
+
+        public async Task<IEnumerable<Result>> GetResultsAsync(int examId)
+        {
+            var results = await _context.Results.Include(x => x.Question).Where(r => r.Question.ExamId == examId).ToListAsync();
+
+            return results;
+        }
+
+        public async Task<Result> UpdateResultAsync(Result result)
+        {
+            _context.Results.Update(result);
+            await _context.SaveChangesAsync();
+
+            return result;
+        }
+    }
+}
